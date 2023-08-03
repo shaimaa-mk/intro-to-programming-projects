4b923f7ec5a68c49e52eab03008424e5cfc6afe3
shaimaa-mk <sh.khwork2020@gmail.com>
2023-08-04 01:13:03 +0300

    Add input validation for text and numbers.

--- adventure_game.py
+++ adventure_game.py
@@ -49,58 +49,67 @@ def print_pause(message, delay=1):
     typing_simulator(chosen_color + message)
 
 
+def string_input(prompt_, options_):
+    """
+    String Input checks player's input as alphabetic validation
+    :param prompt_: string that represents a type of input request.
+    :param options_: list that consists of which data player
+    is only allowed to enter.
+    :return: string that represents one of given options.
+    """
+    while True:
+        option_ = input(prompt_).lower()
+        if option_.isalpha() and option_ in options_:
+            return option_
+        else:
+            print(f"Only Alphabetic values! {options_}")
+
+
 def play_again():
     """
     Play Again gives the player the ability to continue playing
     or just get off the game.
     """
+    play_ = string_input("Would you like to play again? (y/n)", ['y', 'n'])
 
-    play_ = input("Would you like to play again? (y/n)")
-
-    if play_ == 'y':
-        # here the player has another chance to a new adventure lol ...
-        print_pause("Excellent! Restarting the game ...")
-        start_adventure()
-
-    elif play_ == 'n':
+    if play_ == 'n':
         # at this point the player gets off the whole game
         print_pause("Thanks for playing! See you next time.")
-        return
-    else:
-        # In case player enters somthing rather than y or n,
-        # program is still running and pops the same question up to the player.
-        play_again()
+        exit(0)
+    elif play_ == 'y':
+        # here the player has another chance to a new adventure lol ...
+        print_pause("Excellent! Restarting the game ...")
 
 
-def fighting_action(weapon_6, beast_5):
+def fighting_action(weapon_4, beast_3):
     """
     Fighting Action is where the player decided to fight with
     weak weapon or the Sword of Ogoroth.
-    :param weapon_6: string that represents the name of the current
+    :param weapon_4: string that represents the name of the current
     weapon player has.
-    :param beast_5:string that represents a random name of beast
+    :param beast_3:string that represents a random name of beast
     that player will fight.
     """
     # In case the player action is fighting with Sword of Ogoroth
     # then the following instructions display.
-    if 'Sword of Ogoroth' in weapon_6:
-        print_pause(f"As the {beast_5} moves to attack, "
+    if 'Sword of Ogoroth' in weapon_4:
+        print_pause(f"As the {beast_3} moves to attack, "
                     f"you unsheath your new sword.")
 
         print_pause("The Sword of Ogoroth shines brightly in "
                     "your hand as you brace yourself for the attack.", 2)
 
-        print_pause(f"But the {beast_5} takes one look at "
+        print_pause(f"But the {beast_3} takes one look at "
                     f"your shiny new toy and runs away!", 3)
 
-        print_pause(f"You have rid the town of the {beast_5}. "
+        print_pause(f"You have rid the town of the {beast_3}. "
                     f"You are victorious!", 2)
 
     # In case the player action is fighting with weak weapon
     # then the following instructions display.
     else:
         print_pause("You do your best...")
-        print_pause(f"but your {weapon_6} is no match for the {beast_5}.")
+        print_pause(f"but your {weapon_4} is no match for the {beast_3}.")
         print_pause("You have been defeated!", 2)
 
 
@@ -113,57 +122,41 @@ def running_away_action():
                 "you don't seem to have been followed.")
 
 
-def house_movement(weapon_5, beast_4):
+def house_movement(weapon_3, beast_2):
     """
     House Movement instructs the player moving after
     knocking the door of the house.
-    :param weapon_5: string that represents the name of
+    :param weapon_3: string that represents the name of
     the current weapon player has.
-    :param beast_4: string that represents a random name
+    :param beast_2: string that represents a random name
     of beast that player will fight.
     """
     # In case player choose to knock on the door of the house
     # then the following instructions display.
     print_pause("You approach the door of the house.")
     print_pause(f"You are about to knock when the door opens a"
-                f"nd out steps a {beast_4}.", 2)
+                f"nd out steps a {beast_2}.", 2)
 
-    print_pause(f"Eep! This is the {beast_4}'s house!", 2)
-    print_pause(f"The {beast_4} attacks you!")
-    if 'Sword of Ogoroth' not in weapon_5:
+    print_pause(f"Eep! This is the {beast_2}'s house!", 2)
+    print_pause(f"The {beast_2} attacks you!")
+    if 'Sword of Ogoroth' not in weapon_3:
         print_pause(f"You feel a bit under-prepared for this, "
-                    f"what with only having a {weapon_5}.", 3)
+                    f"what with only having a {weapon_3}.", 3)
 
-    # another action that player need to take is move_2
-    move_2 = input("Would you like to (1) fight or (2) run away?")
-
-    # the player decided to fight
-    if move_2 == '1':
-        fighting_action(weapon_5, beast_4)
-        # after player won or defeated, another chance to play is given though
-        play_again()
-
-    # the player decided to run away
-    elif move_2 == '2':
-        running_away_action()
-        # the game is still on and the player has back to the beginning
-        play(weapon_5, beast_4)
-
-    # the player get stuck with incorrect input
-    else:
-        play_again()
+    return numeric_input("Would you like to (1) fight or (2) run away?",
+                         [1, 2])
 
 
-def cave_movement(weapon_4):
+def cave_movement(weapon_2):
     """
     Cave Movement instructs where the player is already
     peering the small cave.
-    :param weapon_4: string that represents the name of
+    :param weapon_2: string that represents the name of
     the current weapon player has.
     """
     print_pause("You peer cautiously into the cave.")
 
-    if weapon_4 == 'Sword of Ogoroth':
+    if weapon_2 == 'Sword of Ogoroth':
         print_pause("You've been here before, and gotten all "
                     "the good stuff. It's just an empty cave now.", 2)
 
@@ -171,67 +164,48 @@ def cave_movement(weapon_4):
         print_pause("It turns out to be only a very small cave.", 2)
         print_pause("Your eye catches a glint of metal behind a rock.", 3)
         print_pause("You have found the magical Sword of Ogoroth!", 2)
-        print_pause(f"You discard your silly old {weapon_4} "
+        print_pause(f"You discard your silly old {weapon_2} "
                     f"and take the sword with you.", 3)
 
     print_pause("You walk back out to the field.", 2)
 
 
-def main_movement(weapon_3, beast_3):
+def numeric_input(prompt, options):
     """
-    Main Movement is the center of the adventure game
-    where player moves(removes).
-    :param weapon_3: string that represents the name
-    of the current weapon player has.
-    :param beast_3: string that represents a random
-    name of beast that player will fight.
+    Numeric Input checks player's input as numeric validation
+    :param prompt: string that represents a request of input data.
+    :param options: list that consists of which data player
+    is only allowed to enter.
+    :return: integer that represents one of given options.
     """
-    # main move player takes will be move_1
-    move_1 = input("(Please enter 1 or 2.)\n")
-
-    if move_1 == '1':
-        house_movement(weapon_3, beast_3)
-
-    elif move_1 == '2':
-        cave_movement(weapon_3)
-        play('Sword of Ogoroth', beast_3)
-
-    else:
-        main_movement(weapon_3, beast_3)
+    while True:
+        option = input(prompt).lower()
+        if option.isnumeric() and int(option) in options:
+            print("yeah")
+            return int(option)
+        else:
+            print(f"Only numeric values! {options}")
 
 
-def main_movement_instructions():
+def main_movement():
     """
-    Main Movement Instructions gives the player main choices of
-    the adventure game, which are:
-        1: Enter 1 to knock on the door of the house.
-        2: Enter 2 to peer into the cave.
+    Main Movement gives the player the main choices of the game.
+    :return: integer that represents one of the given options
     """
     print_pause("\nEnter 1 to knock on the door of the house.")
     print_pause("Enter 2 to peer into the cave.")
     print_pause("What would you like to do?")
+    return numeric_input("(Please enter 1 or 2.)\n", [1, 2])
 
 
-def play(weapon_2, beast_2):
+def adventure_story(beast_1, weapon_1):
     """
-    Play is where the player is in the center of the game.
-    :param weapon_2:string that represents the name of
-    the current weapon player has.
-    :param beast_2: string that represents a random name
-    of beast that player will fight.
-    """
-    # call main_movement_instructions so the player is led
-    # by adventure game guidelines
-    main_movement_instructions()
-
-    # call main_movement so the player can take an action.
-    main_movement(weapon_2, beast_2)
-
-
-def intro_adventure(beast_1, weapon_1):
-    """
-    Intro Adventure makes the beginning of the adventure
+    Adventure Story makes the beginning of the adventure
     game which is telling the story of the adventure ...
+    :param beast_1: string that represents a random name
+    of beast that player will fight.
+    :param weapon_1: string that represents the name of
+    the current weapon player has.
     """
     print_pause("You find yourself standing in an open field, "
                 "filled with grass and yellow wildflowers.")
@@ -243,17 +217,41 @@ def intro_adventure(beast_1, weapon_1):
                 f" {weapon_1}.", 2)
 
 
-def start_adventure():
+def play():
     """
-    Start Adventure is the point of whole adventure game
+    Play is where the player is in the center of the game.
     """
     # make randomness so the game become more enjoyable ...
-    beast_ = random.choice(['troll', 'dragon', 'wicked fairie', 'pirate'])
-    weapon_ = random.choice(['tiny dagger', 'tiger claws', 'finger knife'])
-    # the player get started new adventure ...
-    intro_adventure(beast_, weapon_)
-    # the player start the game
-    play(weapon_, beast_)
+    beast = random.choice(['troll', 'dragon', 'wicked fairie', 'pirate'])
+    weapon = random.choice(['tiny dagger', 'tiger claws', 'finger knife'])
+    # tell the player which adventure it is ...
+    adventure_story(beast, weapon)
+    # start moving around
+    while True:
+        move_1 = main_movement()
+        if move_1 == 1:
+            move_2 = house_movement(weapon, beast)
+            if move_2 == 1:
+                fighting_action(weapon, beast)
+                break
+            else:
+                running_away_action()
+        else:
+            cave_movement(weapon)
+            weapon = 'Sword of Ogoroth'
 
 
-start_adventure()
+def adventure_game():
+    """
+    Adventure Game is the center of the game where the player
+    goes through a different adventure each time.
+    """
+    while True:
+        # the player get started new adventure ...
+        play()
+        # the player gets a chance to play again ...
+        play_again()
+
+
+if __name__ == '__main__':
+    adventure_game()
